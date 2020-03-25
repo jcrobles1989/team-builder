326e48f9a3a4ee9d84b546c00be3aad15c908ae1
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-03-25 14:04:05 -0700

    almost finished

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form';
+
+const initialTeamMembers = [
+  {firstName: 'John', lastName: 'Smith'},
+  {firstName: 'Jane', lastName: 'Doe'}
+]
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState(initialTeamMembers)
+  const [formValues, setFormValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    role: ''
+  })
+  const onInputChange = event => {
+    const inputThatChanged = event.target.name
+    const newValueForInput = event.target.value
+    setFormValues({
+      ...formValues,
+      [inputThatChanged]: newValueForInput,
+    })
+  }
+  const onFormSubmit = event => {
+    event.preventDefault()
+    const newTeamMember = {
+      // id: uuid(),
+      firstName: formValues.firstName,
+      lastName: formValues.lastName,
+      email: formValues.email,
+      role: formValues.role
+    }
+    setTeamMembers([...teamMembers, newTeamMember])
+    // setTeamMembers(teamMembers.concat(newTeamMember))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Team Members:</h1>
+  {teamMembers.map(item => <h3 key={item.id}>{item.firstName} {item.lastName} {item.email} {item.role}</h3>)}
+        
+      <Form 
+        onInputChange={onInputChange}
+        formValues={formValues}
+        onFormSubmit={onFormSubmit}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
